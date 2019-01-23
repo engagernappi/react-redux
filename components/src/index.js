7ef7a8ecad6698d0cd23c2e1d1bddd3ccd3bcc9d
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-01-23 10:26:08 -0200

    36.Component reuse

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApprovalCard from './ApprovalCard';
 import CommentDetail from './CommentDetail'
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard/>
             <CommentDetail avatar={faker.image.avatar()} author="Sam" content="Nice blog post! 1" timeAgo="Today at 6:00PM" />
             <CommentDetail avatar={faker.image.avatar()} author="Alex" content="Nice blog post! 2" timeAgo="Today at 4:00PM" />
             <CommentDetail avatar={faker.image.avatar()} author="Jane" content="Nice blog post! 3" timeAgo="Today at 2:00AM" />
