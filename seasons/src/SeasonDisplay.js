87d9f8d94389e9fb8cce7ae2c57421ff55ef98d9
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-01-24 13:58:27 -0200

    68.Specifying default props

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,20 +1,34 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        iconName: 'sun',
+        text: 'Let\'s hit the beach'
+    },
+    winter: {
+        iconName: 'snowflake',
+        text: 'Burr, it is cold!'
+    }
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
-    } else { 
+    } else {
         return lat > 0 ? 'winter' : 'summer';
     }
 };
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    const { iconName, text } = seasonConfig[season];
 
     return (
-        <div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -21,7 +22,7 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>;
         }
 
-        return <div>Loading!</div>;
+        return <Spinner message="Please accept the location permission."/>;
     }
 }
 
