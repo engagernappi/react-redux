687ef864de85ab61bc55c90d55184490e9a08b14
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-01-25 17:19:01 -0200

    106.App review

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 class ImageCard extends React.Component {
     imageRef = React.createRef();
+    state = { spans: 0 };
 
     componentDidMount() {
         this.imageRef.current.addEventListener('load', this.setSpan);
     }
 
     setSpan = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const spans = Math.ceil(this.imageRef.current.clientHeight / 10);
+        this.setState({ spans });
     }
 
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div className="image-card">
+            <div className="image-card" style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} src={urls.regular} alt={description} />
             </div>
         );
