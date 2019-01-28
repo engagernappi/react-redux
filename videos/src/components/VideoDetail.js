f4fe49335a4a0df12a3b68d3eb5e3ec47f618dcb
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-01-28 14:54:40 -0200

    Displaying a video player

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
-    if(!video){
+    if (!video) {
         return <div>Loading...</div>
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
-        <div className="video-detail ui segment">
-            <h4 className="ui header">{video.snippet.title}</h4>
-            <p>{video.snippet.description}</p>
+        <div className="video-detail">
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     );
 }
