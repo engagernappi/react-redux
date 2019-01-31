f0a6bb05e0d7c189c93cff1fa4baf585524dba17
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-01-31 13:52:40 -0200

    164.Shirtened syntax with redux thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 //Action creator
 export const fetchPost = () => {
-    return {
-        type: 'FETCH_POST'
+    return async function (dispatch) {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({
+            type: 'FETCH_POST',
+            payload: response
+        });
     };
 };
