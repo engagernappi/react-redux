eaff36c5cf6d2c983a8bec470d28bf943f6ae72d
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-02-02 10:58:21 -0200

    220.Recording the user´s id

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -20,7 +20,7 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = (isSignedIn) => {
         if (isSignedIn) {
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         } else {
             this.props.signOut();
         }
@@ -62,6 +62,7 @@ class GoogleAuth extends React.Component {
 };
 
 const mapStateToProps = (state) => {
+    console.log(state);
     return {
         isSignedIn: state.auth.isSignedIn
     };
