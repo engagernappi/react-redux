05b12d1897b75ae6772d5b28425d80fcc2044622
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-02-06 12:12:03 -0200

    295.A touch more setup

---
+++ translate/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+    render(){
+        return(
+            <button className="ui button primary">Create</button>
+        );
+    }
+}
+
+export default Button;
