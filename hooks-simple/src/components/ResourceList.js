2dc7163dca24a9732e578bf4ffce4f372b2ec2a2
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-02-06 16:57:39 -0200

    319.Refactoring a class to a function

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-    state = { resources: [] };
+const ResourceList = () => {
+    const [resources, setResources] = useState([]);
 
-    async componentDidMount() {
+    const fetchResources = async () => {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-        this.setState({ resources: response.data });
+        setResources(response.data);
     }
 
-    render() {
-        return (
-            <div>{this.state.resources.length}</div>
-        );
-    }
+    return (
+        <div>{resources.length}</div>
+    );
 }
 
 export default ResourceList;
