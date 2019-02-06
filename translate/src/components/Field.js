e0bf0b20ef0b77344c3494304eb7abca63909b35
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-02-06 15:30:38 -0200

    309.Context vs Redux Recap

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
     static contextType = LanguageContext;
     
     render() {
-        const text = this.context === 'english' ? 'Name' : 'Dutch name';
+        const text = this.context.language === 'english' ? 'Name' : 'Dutch name';
         return (
             <div className="ui field">
                 <label>{text}</label>
