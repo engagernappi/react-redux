cf8ce46dcbb1b96d1fba3f6c48bfda55eb4dfa90
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2019-02-07 09:03:14 -0200

    331.Refactoring a class to a function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
+            position => setLat(position.coords.latitude),
+            err => setErrorMessage(err.message)
         );
+    }, []);
+
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <Spinner message="Please accept the location permission." />;
     }
 
-    renderContent() {
-        if (this.state.lat && !this.state.errorMessage) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        if (!this.state.lat && this.state.errorMessage) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-
-        return <Spinner message="Please accept the location permission." />;
-    }
-
-    render() {
-        return <div>{this.renderContent()}</div>
-    }
-}
+    return <div className="border red">{content}</div>
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
